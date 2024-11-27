7253efdc9c3c608f22fcfb70af09c95d0b69edee
Prashant Sahni <prashant.sahni5@gmail.com>
2024-11-27 14:15:57 +0530

    delete todo

--- app/store/TodoStore.tsx
+++ app/store/TodoStore.tsx
@@ -4,6 +4,7 @@ import { ITodo } from '@/app/types'
 export type TodoState = {
   todos: ITodo[]
   addTodo: (todo: string) => void;
+  deleteTodo: (id: number) => void;
 }
 
 export const useTodoStore = create<TodoState>()((set) => ({
@@ -13,6 +14,7 @@ export const useTodoStore = create<TodoState>()((set) => ({
     { id: 3, todo: "Buy eggs" },
   ],
   addTodo: (todo: string) => set((state: { todos: ITodo[]}) => ({ todos: [...state.todos, { id: state.todos.length + 1, todo }]})),
+  deleteTodo: (id: number) => set((state: { todos: ITodo[]}) => ({ todos: state.todos.filter((todo) => todo.id !== id)})),
 }))
 
 export default useTodoStore;
